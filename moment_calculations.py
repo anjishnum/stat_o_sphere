8e765e88d5cf7e3e16405022107a4d856c14ba01
Anjishnu <anjishnumahanta@gmail.com>
2020-08-25 00:39:21 +0530

    modify to include -ve and higher digits numbers

--- moment_calculations.py
+++ moment_calculations.py
@@ -1,17 +1,18 @@
 '''1st order to 4th order i.e. population mean to population kurtosis calculation'''
 
+import numpy as np
+
 class moment:
 
     def __init__(self):
         a = input("Enter array: ")
-        ar = []
-        for i in a:
-            try:
-                type(int(i))
-                ar.append(int(i))
-            except:
-                continue
-        print(self.mean(ar))
+        a = a.split(',')
+        ar = [float(x) for x in a]
+        #print(ar) //for debugging if necessary
+        print("Population mean: ",round(self.mean(ar),2))
+        print("Population variance: ",round(self.variance(ar),2))
+        print("Population skewness: ",round(self.skewness(ar),2))
+        print("Population kurtosis: ",round(self.kurtosis(ar),2))
 
     # Population mean
     def mean(self,ar):
@@ -22,6 +23,42 @@ class moment:
             value+=i
         
         value /= len(ar)
+
+        return value
+
+    # Population variance
+    def variance(self,ar):
+        
+        value = 0
+
+        for i in ar:
+            value += (i-self.mean(ar))**2
+
+        value /= len(ar)
+
+        return value
+
+    # Population skewness
+    def skewness(self,ar):
+        
+        value = 0
+
+        for i in ar:
+            value += np.power((i-self.mean(ar)),3)
+
+        value /= len(ar)*(np.power(self.variance(ar),3))
+
+        return value
+
+    # Population kurtosis
+    def kurtosis(self,ar):
+
+        value = 0
+
+        for i in ar:
+            value += np.power((i-self.mean(ar)),4)
+
+        value /= len(ar)*(np.power(self.variance(ar),4))
 
         return value
 
